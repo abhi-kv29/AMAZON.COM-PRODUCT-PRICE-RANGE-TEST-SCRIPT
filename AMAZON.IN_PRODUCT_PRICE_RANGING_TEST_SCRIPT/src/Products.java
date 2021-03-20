3e56f52a58d8db8de892f707f17e772313b5275d
abhi-kv29 <keshav.shm@gmail.com>
2021-03-20 16:28:38 +0530

    renamed files

--- AMAZON.IN_PRODUCT_PRICE_RANGING_TEST_SCRIPT/src/Products.java
+++ AMAZON.IN_PRODUCT_PRICE_RANGING_TEST_SCRIPT/src/Products.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class productRating {
+public class Products {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
